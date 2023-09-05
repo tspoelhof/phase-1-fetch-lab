5ad60dee97e4d6b208f93389de1fd254459abc51
Thomas Spoelhof <thomasjspoelhof@gmail.com>
2023-09-04 21:47:27 -0400

    json GOT

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((data) => {
+  renderBooks(data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
 }
 
 function renderBooks(books) {
